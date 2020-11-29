fb1ff27c003d76781857e401b7951946558250c3
KPM42 <54440854+KPM42@users.noreply.github.com>
2020-11-29 14:58:11 +0500

    Update SuffixTree.cpp

--- Strings/SuffixTree.cpp
+++ Strings/SuffixTree.cpp
@@ -1,81 +1,126 @@
-#include <bits/stdc++.h>
 
-using namespace std;
+int posnow = 0;
+int need_spusk = 0;
+int curv = 0;
 
-#define fpos adla
-const int inf = 1e9;
-const int maxn = 1e4;
-char s[maxn];
-map<int, int> to[maxn];
-int len[maxn], fpos[maxn], link[maxn];
-int node, pos;
-int sz = 1, n = 0;
+struct node {
+    map<int, int> to; // ребра из вершины
+    // len и pos описывает ребро в эту вершину
+    int len;
+    int pos;
+    int sufflink;
+    node() {
+        len = 0;
+        pos = 0;
+        sufflink = 0;
+    }
+};
 
-int make_node(int _pos, int _len)
-{
-    fpos[sz] = _pos;
-    len [sz] = _len;
-    return sz++;
-}
+node SuffixTree[1000000];
+int text[1000000];
+int cnt = 1;
 
-void go_edge()
-{
-    while(pos > len[to[node][s[n - pos]]])
-    {
-        node = to[node][s[n - pos]];
-        pos -= len[node];
+void go_edges() {
+    int e = text[posnow - need_spusk];
+    int nextv = SuffixTree[curv].to[e];
+    while (need_spusk > SuffixTree[nextv].len) {
+        need_spusk -= SuffixTree[nextv].len;
+        curv = nextv;
+        e = text[posnow - need_spusk];
+        nextv = SuffixTree[curv].to[e];
     }
 }
 
-void add_letter(int c)
-{
-    s[n++] = c;
-    pos++;
+int make_node(int pos, int len) {
+    SuffixTree[cnt].pos = pos;
+    SuffixTree[cnt].len = len;
+    cnt++;
+    return cnt - 1;
+}
+
+int nowq = 0; 
+void add_letter(int c) {
+    text[posnow] = c;
+    posnow++;
+    need_spusk++;
     int last = 0;
-    while(pos > 0)
-    {
-        go_edge();
-        int edge = s[n - pos];
-        int &v = to[node][edge];
-        int t = s[fpos[v] + pos - 1];
-        if(v == 0)
-        {
-            v = make_node(n - pos, inf);
-            link[last] = node;
+    while (need_spusk > 0) {
+        go_edges();
+        int e = text[posnow - need_spusk]; // по какому ребру спускаемся
+        int to = SuffixTree[curv].to[e];
+        int t = text[SuffixTree[to].pos + need_spusk - 1]; // какая буква на той позиции в которую спусимся по ребру
+        if (to == 0) { // мы уже стоим в вершине и надо отпочковать лист
+            SuffixTree[curv].to[e] = make_node(posnow - need_spusk, inf); // отпочковали литст
+            SuffixTree[last].sufflink = curv; // поставили ссылку
             last = 0;
         }
-        else if(t == c)
-        {
-            link[last] = node;
+        else if (t == c) { // если этот символ уже есть, то ставим ссылку и завершаем итерацию
+            SuffixTree[last].sufflink = curv; //
             return;
         }
-        else
-        {
-            int u = make_node(fpos[v], pos - 1);
-            to[u][c] = make_node(n - 1, inf);
-            to[u][t] = v;
-            fpos[v] += pos - 1;
-            len [v] -= pos - 1;
-            v = u;
-            link[last] = u;
-            last = u;
+        else {
+            // сначала крафтим разветвление у которого сейчас номер cnt
+            int wasto = to; // в середине какого ребра будет развилка
+            int razvetvlenie = make_node(SuffixTree[wasto].pos, need_spusk - 1); // крафтим разветвление
+            SuffixTree[razvetvlenie].to[c] = make_node(posnow - 1, inf); // из него ответвляем лист
+            SuffixTree[razvetvlenie].to[t] = wasto; // и ребро в восту
+            SuffixTree[wasto].len -= need_spusk - 1; // теперь ребро в нее уменьшилось
+            SuffixTree[wasto].pos += need_spusk - 1; // теперь ребро из разветвления, которое начинается на нид спуск позже
+            SuffixTree[curv].to[e] = razvetvlenie; // теперь из курвы мы идем по этому символу в разветвление
+            SuffixTree[last].sufflink = razvetvlenie;
+            last = razvetvlenie;
         }
-        if(node == 0)
-            pos--;
+        if (curv == 0)
+            need_spusk--;
         else
-            node = link[node];
+            curv = SuffixTree[curv].sufflink;
     }
 }
 
-int main()
-{
-    len[0] = inf;
-    string s;
-    cin >> s;
-    int ans = 0;
-    for(int i = 0; i < s.size(); i++)
-        add_letter(s[i]);
-    for(int i = 1; i < sz; i++)
-        ans += min((int)s.size() - fpos[i], len[i]);
-    cout << ans << "\n";
+
+bool is_substring(string s) { // есть ли такая подстрока
+    int nowv = 0;
+    int pos = 0;
+    int to = SuffixTree[0].to[s[0] - 'a'];
+    if (to == 0) {
+        return false;
+    }
+    int gone = 0;
+    while (true) {
+        if (pos == s.length())
+            return true;
+        int c = s[pos] - 'a';
+        int t = text[SuffixTree[to].pos + gone];
+        if (t != c) {
+            return false;
+        }
+        gone++;
+        pos++;
+        if (pos == s.length()) {
+            return true;
+        }
+        if (gone == SuffixTree[to].len && pos != s.length()) {
+            int e = s[pos] - 'a';
+            to = SuffixTree[to].to[e];
+            gone = 0;
+            if (to == 0) {
+                return false;
+            }
+        }
+    }
+}
+
+void AddWord(string& s) {
+    need_spusk = 0;
+    curv = 0;
+    int n = s.length();
+    for (int i = 0; i < n; i++) {
+        add_letter(s[i] - 'a');
+    }
+    add_letter(100 + nowq);
+    nowq++;
+}
+
+void solve() {
+    SuffixTree[0].len = inf;
 }
