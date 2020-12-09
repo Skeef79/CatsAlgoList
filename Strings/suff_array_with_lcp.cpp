d42037cf88e85bec2b74fa07041a0c7823f2573d
Igor Subbotin <theskeef79@gmail.com>
2020-12-09 22:15:16 +0500

    Create suff_array_with_lcp.cpp

---
+++ Strings/suff_array_with_lcp.cpp
@@ -0,0 +1,155 @@
+struct sparceTable
+{
+	const int K = 20;
+	int n;
+	vector<vector<int>>t;
+	vector<int> log;
+	function<int(int, int)> f;
+
+	void build(vector<int>&a)
+	{
+		for (int i = 0; i < n; i++)
+			t[i][0] = a[i];
+
+		for (int j = 1; j < K; j++)
+			for (int i = 0; i <= n - (1 << j); i++)
+				t[i][j] = f(t[i][j - 1], t[i + (1 << (j - 1))][j - 1]);
+
+		log[1] = 0;
+		for (int i = 2; i <= n; i++)
+			log[i] = log[i / 2] + 1;
+	}
+
+	void init(vector<int>&a, function<int(int, int)> _f)
+	{
+		f = _f;
+		n = a.size();
+		t = vector<vector<int>>(n, vector<int>(K));
+		log = vector<int>(n + 1);
+		build(a);
+	}
+
+	sparceTable() {}
+	
+	sparceTable(vector<int>&a, function<int(int, int)>_f) 
+	{ 
+		init(a, _f); 
+	}
+
+	int get(int l, int r)
+	{
+		int j = log[r - l + 1];
+		return f(t[l][j], t[r - (1 << j) + 1][j]);
+	}
+
+};
+
+int _min(int a, int b)
+{
+	return min(a, b);
+}
+
+struct suffixArray
+{
+	int n;
+	vector<int>suffix, c, cnt, pos, lcp;
+	string s;
+	sparceTable rmq;
+
+	suffixArray(string _s)
+	{
+		s = _s;
+		s += "$";
+		n = s.length();
+		suffix.resize(n);
+		c.resize(n);
+		cnt.resize(n);
+		pos.resize(n);
+		lcp.resize(n);
+
+		buildArray(s);
+		buildLCP();
+	}
+
+	void buildArray(string s)
+	{
+		iota(all(suffix), 0);
+		sort(all(suffix), [&](int a, int b) { return s[a] < s[b]; });
+		for (int i = 1; i < n; i++)
+			c[suffix[i]] = (s[suffix[i]] == s[suffix[i - 1]] ? c[suffix[i - 1]] : i);
+
+		for (int k = 1; k < n; k *= 2)
+		{
+			iota(all(cnt), 0);
+			vector<int>new_c(c), new_suffix(suffix);
+			for (int i = 0; i < n; i++)
+			{
+				int j = (new_suffix[i] - k + n) % n;
+				suffix[cnt[new_c[j]]++] = j;
+			}
+			c[suffix[0]] = 0;
+			for (int i = 1; i < n; i++)
+			{
+				bool same = new_c[suffix[i]] == new_c[suffix[i - 1]]
+					&& new_c[(suffix[i] + k) % n] == new_c[(suffix[i - 1] + k) % n];
+				c[suffix[i]] = same ? c[suffix[i - 1]] : i;
+			}
+			for (int i = 0; i < n; i++)
+				pos[suffix[i]] = i;
+		}
+	}
+
+	//returns 2 if t<suff[pos], 1 is t==suff[pos], else 0
+	int cmpStrings(int pos, string &t)
+	{
+		for (int i = 0; i < min(n - pos, (int)t.size()); i++)
+			if (t[i] != s[pos + i])
+				if (t[i] < s[pos + i])
+					return 2;
+				else
+					return 0;
+
+		if (t.length() > n - pos)
+			return 0;
+
+		return 1;
+	}
+
+	//returns first occurrence of the string t, else - 1;
+	int find(string &t)
+	{
+		int l = 0, r = n - 1;
+		while (r - l > 1)
+		{
+			int m = (l + r) / 2;
+			if (cmpStrings(suffix[m], t))
+				r = m;
+			else
+				l = m;
+		}
+		return cmpStrings(suffix[r], t) == 1 ? suffix[r] : -1;
+	}
+
+	void buildLCP()
+	{
+		int k = 0;
+		for (int i = 0; i < n - 1; i++)
+		{
+			while (s[i + k] == s[suffix[pos[i] - 1] + k])
+				k++;
+			lcp[pos[i]] = k;
+			k = max(k - 1, 0);
+		}
+		rmq.init(lcp, _min);
+	}
+
+	int get_lcp(int i, int j)
+	{
+		i = pos[i], j = pos[j];
+		if (i > j)
+			swap(i, j);
+
+		return rmq.get(i + 1, j);
+	}
+
+};
