b9bb349ae772350f07548d34d915aeca0e98c658
Igor Subbotin <theskeef79@gmail.com>
2021-03-13 23:09:52 +0500

    Create extended_gcd.cpp

---
+++ Number Theory/extended_gcd.cpp
@@ -0,0 +1,14 @@
+ll gcd(ll a, ll b, ll &x, ll &y)
+{
+	if (b == 0)
+	{
+		x = 1;
+		y = 0;
+		return a;
+	}
+	ll x1, y1;
+	ll g = gcd(b, a%b, x1, y1);
+	x = y1;
+	y = x1 - (a / b) * y1;
+	return g;
+}
