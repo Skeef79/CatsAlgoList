37cf79e81d94d43a3d6a76df8290166765beb11a
Igor Subbotin <theskeef79@gmail.com>
2021-04-16 15:13:14 +0500

    Update suffix_array_log2.cpp

--- Strings/suffix_array_log2.cpp
+++ Strings/suffix_array_log2.cpp
@@ -1,12 +1,10 @@
-struct suffixArray
-{
+struct suffixArray {
 	int n;
 	vector<int>suffix, c;
 	vector<pii>tmp;
 	string s;
 
-	suffixArray(string _s)
-	{
+	suffixArray(string _s) {
 		s = _s;
 		n = s.length()+1;
 		suffix = vector<int>(n);
@@ -15,18 +13,15 @@ struct suffixArray
 		build(_s);
 	}
 
-	void build(string s)
-	{
+	void build(string s) {
 		s += "$";
 		iota(all(suffix), 0);
 		sort(all(suffix), [&](int a, int b) { return s[a] < s[b]; });
 		for (int i = 1; i < n; i++)
 			c[suffix[i]] = c[suffix[i - 1]] + (s[suffix[i]] != s[suffix[i - 1]]);
 
-		for (int k = 1; k < n; k *= 2)
-		{
-			sort(all(suffix), [&](int a, int b)
-			{ 
+		for (int k = 1; k < n; k *= 2) {
+			sort(all(suffix), [&](int a, int b) { 
 				return make_pair(c[a], c[(a + k) % n]) < make_pair(c[b], c[(b + k) % n]);
 			});
 
