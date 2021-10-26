ffed21d217d5897e88aece88daf9a9f7766c7033
Igor Subbotin <theskeef79@gmail.com>
2021-10-26 20:57:29 +0500

    Update bridges.cpp

--- Graphs/bridges.cpp
+++ Graphs/bridges.cpp
@@ -5,19 +5,16 @@ int timer = 0;
 
 vector<pii> bridges;
 
-void dfs(int v, int p = -1)
-{
+void dfs(int v, int p = -1) {
 	used[v] = true;
 	tin[v] = fup[v] = timer++;
-	for (auto to : g[v])
-	{
+	for (auto to : g[v]) {
 		if (to == p)
 			continue;
 
 		if (used[to])
 			fup[v] = min(fup[v], tin[to]);
-		else
-		{
+		else {
 			dfs(to, v);
 			fup[v] = min(fup[v], fup[to]);
 			if (fup[to] > tin[v])
