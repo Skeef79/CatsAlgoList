340348575439236b84ad3d54e3223048627fad8e
Igor Subbotin <theskeef79@gmail.com>
2021-10-26 20:13:36 +0500

    Update lca_binary_lifting.cpp

--- Graphs/lca_binary_lifting.cpp
+++ Graphs/lca_binary_lifting.cpp
@@ -1,46 +1,39 @@
 const int LOG = 20;
-int n;
+vec<int>lvl, tin, tout;
+vec<vec<int>>g;
+vec<vec<int>>jump;
 
-vector<vector<int>> g;
-vector<int> lvl;
-vector<vector<int>> jump;
+int timer = 0;
 
-void dfs(int v, int level=0, int p = 0)
-{
+void dfs(int v, int p = 0, int level = 0) {
 	lvl[v] = level;
 	jump[v][0] = p;
+	tin[v] = timer++;
 
-	for (auto to : g[v])
-		if (to != p)
-			dfs(to, level + 1, v);
+	for (int k = 1; k < LOG; k++)
+		jump[v][k] = jump[jump[v][k - 1]][k - 1];
+
+	for (auto to : g[v]) {
+		if (to == p)
+			continue;
+		dfs(to, v);
+	}
+	tout[v] = timer++;
 }
 
-void InitJump()
-{
-	for (int j = 1; j < LOG; j++)
-		for (int v = 0; v < n; v++)
-			jump[v][j] = jump[jump[v][j - 1]][j - 1];
-
+bool isAncestor(int u, int v) {
+	return tin[u] <= tin[v] && tout[u] >= tout[v];
 }
 
-int lca(int u, int v)
-{
-	if (lvl[u] > lvl[v])
-		swap(u, v);
-
-	for (int k = LOG - 1; k >= 0; k--)
-		if (lvl[jump[v][k]] >= lvl[u])
-			v = jump[v][k];
-
-	if (v == u)
+int lca(int u, int v) {
+	if (isAncestor(u, v))
 		return u;
-
+	if (isAncestor(v, u))
+		return v;
+	
 	for (int k = LOG - 1; k >= 0; k--)
-		if (jump[v][k] != jump[u][k])
-		{
+		if (!isAncestor(jump[v][k], u))
 			v = jump[v][k];
-			u = jump[u][k];
-		}
 
-	return jump[u][0];
+	return jump[v][0];
 }
