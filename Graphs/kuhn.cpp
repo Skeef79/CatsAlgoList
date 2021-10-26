4c47355ad7f8386ac74fd2c9238d576474d6da19
Igor Subbotin <theskeef79@gmail.com>
2021-10-26 20:59:08 +0500

    Update kuhn.cpp

--- Graphs/kuhn.cpp
+++ Graphs/kuhn.cpp
@@ -1,30 +1,26 @@
 vector<vector<int>>g;
-vector<int>r;
+vector<int>mt;
 vector<bool>used;
 
 int n, m; 
 
-bool dfs(int v)
-{
+bool dfs(int v) {
 	if (used[v])
 		return false;
 	used[v] = true;
 	
 	for(auto to:g[v])
-		if (r[to] == -1 || dfs(r[to]))
-		{
-			r[to] = v;
+		if (mt[to] == -1 || dfs(mt[to])) {
+			mt[to] = v;
 			return true;
 		}
 	return false;
 }
 
-int kuhn()
-{
+int kuhn() {
 	fill(all(r),-1);
 	int ans = 0;
-	for (int v = 0; v < n; v++)
-	{
+	for (int v = 0; v < n; v++) {
 		fill(all(used), false);
 		if (dfs(v))
 			ans++;
