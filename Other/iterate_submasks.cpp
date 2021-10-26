15c2cb4d94db91a140c511c097feb2b7b254012b
Igor Subbotin <theskeef79@gmail.com>
2021-10-26 20:58:26 +0500

    Update iterate_submasks.cpp

--- Other/iterate_submasks.cpp
+++ Other/iterate_submasks.cpp
@@ -1,19 +1,15 @@
 //подмаски
-for (int mask = 0; mask < (1 << n); ++mask)
-{
-  for (int s = mask; s; s = (s - 1)&mask)
-  {
+for (int mask = 0; mask < (1 << n); ++mask) {
+  for (int s = mask; s; s = (s - 1)&mask) {
     //proceed s;		
   }
 }
 
 //надмаски
-for (int mask = 0; mask < (1 << n); ++mask)
-	{
+for (int mask = 0; mask < (1 << n); ++mask) {
 		int inversion = (1 << n) - 1 & ~mask;
 		//proceed mask first
-		for (int s = inversion; s; s = (s - 1)&inversion)
-		{
+		for (int s = inversion; s; s = (s - 1)&inversion) {
 			int cur_mask = s ^ mask;
 			//proceed cur_mask
 		}
