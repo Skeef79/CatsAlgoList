a5bff544ff69623ba52b5ad88063fd026e0a1bd9
Igor Subbotin <theskeef79@gmail.com>
2021-10-26 20:57:59 +0500

    Update hungarian_algorithm.cpp

--- Graphs/hungarian_algorithm.cpp
+++ Graphs/hungarian_algorithm.cpp
@@ -1,21 +1,17 @@
-pair<ll, vector<int>> hungarianAlgorithm(vector<vector<ll>> a)
-{
+pair<ll, vector<int>> hungarianAlgorithm(vector<vector<ll>> a) {
 	int n = a.size() - 1;
 	int m = a[0].size() - 1;
 
 	vector<int> pl(n + 1, 0), pr(n + 1, 0), mt(n + 1, 0), way(n + 1, 0);
-	for (int row = 1; row <= n; row++) 
-	{
+	for (int row = 1; row <= n; row++) {
 		mt[0] = row;
 		vector<int> minv(n + 1, INF);
 		vector<bool> used(n + 1, false);
 		int col = 0;
-		do 
-		{
+		do {
 			used[col] = true;
 			int v = mt[col], delta = INF, nextcol;
-			for (int i = 1; i <= n; i++) 
-			{
+			for (int i = 1; i <= n; i++) {
 				if (used[i])
 					continue;
 				int x = a[v][i] - pl[v] - pr[i];
@@ -24,8 +20,7 @@ pair<ll, vector<int>> hungarianAlgorithm(vector<vector<ll>> a)
 				if (minv[i] < delta)
 					delta = minv[i], nextcol = i;
 			}
-			for (int i = 0; i <= n; i++) 
-			{
+			for (int i = 0; i <= n; i++) {
 				if (used[i])
 					pl[mt[i]] += delta, pr[i] -= delta;
 				else
@@ -34,8 +29,7 @@ pair<ll, vector<int>> hungarianAlgorithm(vector<vector<ll>> a)
 			col = nextcol;
 		} while (mt[col] != 0);
 
-		do 
-		{
+		do {
 			int nextcol = way[col];
 			mt[col] = mt[nextcol];
 			col = nextcol;
