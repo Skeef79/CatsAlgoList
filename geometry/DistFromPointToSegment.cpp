b131a436e9cf1d4bf7cddc289fc703c4b0255fd9
Dmitriy Sidorenko <dima.sidorenk@yandex.ru>
2022-12-05 16:31:58 +0500

    Update DistFromPointToSegment.cpp

--- geometry/DistFromPointToSegment.cpp
+++ geometry/DistFromPointToSegment.cpp
@@ -1,5 +1,5 @@
 // find dist betweeen point O and segment AB
-ld distToSegment(Point O, Point A, Point B) {
+ld distToSegment(const Point& O, const Point& A, const Point& B) {
     if ((O - A) * (B - A) >= 0 && (O - B) * (A - B) >= 0) {
         return abs((O - A) ^ (B - A)) / (A - B).getLen();
     }
